c00e537999a42516659556601eac07bf0f033bd3
pawaffle <pdegterev@gmail.com>
2024-09-27 11:02:16 +0300

    quik fix

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,4 +1,5 @@
 import { useAuth } from '../context/AuthContext';
+import { Navigate } from 'react-router-dom'
 
 export function ProtectedRoute({ children }) {
   const { isAuthenticated } = useAuth();
